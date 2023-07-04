9ad3b48e0637b27740f681216a0f326d0a1774ec
Admin <admin@MacBook-Pro-von-Admin.local>
2023-07-04 22:40:29 +0200

    First commit

---
+++ public/index.php
@@ -0,0 +1,19 @@
+<?php
+
+include __DIR__ . '/../kirby/vendor/autoload.php';
+// include __DIR__ . '/../kirby/bootstrap.php';
+
+$kirby = new Kirby([
+    'roots' => [
+        'index'    => __DIR__,
+        'base'     => $base    = dirname(__DIR__),
+        'content'  => $base . '/content',
+        'site'     => $base . '/site',
+        'storage'  => $storage = $base . '/storage',
+        'accounts' => $storage . '/accounts',
+        'cache'    => $storage . '/cache',
+        'sessions' => $storage . '/sessions',
+    ]
+]);
+
+echo $kirby->render();
